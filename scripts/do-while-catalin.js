1283ad1a99609d4d7d367a461a99b234fb40503e
CatalinStoichici <117110317+CatalinStoichici@users.noreply.github.com>
2023-05-10 15:04:49 +0300

    github push

---
+++ scripts/do-while-catalin.js
@@ -0,0 +1,28 @@
+/*
+// 1. Afișarea numerelor pare de la 1 la 10
+var i = 1;
+
+do {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+  i++;
+} while (i <= 10);
+
+// 2. Citirea unui număr întreg de la tastatură până când numărul citit este pozitiv
+var numar;
+
+do {
+  numar = prompt("Introdu un număr întreg pozitiv:");
+} while (numar <= 0);
+
+console.log("Ai introdus numărul " + numar);
+*/
+
+// Afisarea numerelor de la 1 pana la 10
+var a = 1;
+
+do{
+  console.log(a);
+  a++;
+}while( a<= 10);
