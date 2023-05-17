6c6f962dc7d93f47ce95fad482dd6f8b62171d5b
CatalinStoichici <stoichicicatalin@gmail.com>
2023-05-17 21:12:10 +0300

    Arrays

---
+++ scripts/arrays.js
@@ -0,0 +1,11 @@
+var carsArray = ['bmw', 'audi', 'renault'];
+
+
+function first(a, b, c){
+   
+    var array =[a,  b, c];
+    console.log(array);
+    var sum = a + b + c;
+    
+    console.log(sum);
+}
