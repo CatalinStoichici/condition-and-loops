01e3834e34c834b0107ffadcf6687fc51f958edc
CatalinStoichici <stoichicicatalin@gmail.com>
2023-05-29 20:32:12 +0300

    DOM

---
+++ dom.js
@@ -0,0 +1,11 @@
+const Button = document.getElementById('myButton');
+
+/*Button.addEventListener('click', function(){
+    console.log("Butonul a fost apasat");
+});*/
+
+
+Button.addEventListener('mouseover', function(){
+    
+    Button.classList.add(myClass);
+});
--- scripts/string.js
+++ scripts/string.js
@@ -20,5 +20,33 @@ var ReturnArray = function splitString(inputword){
 }
 
 /* -------------------------------------------------------------------------- */
-/*                                Exercitiul 2                                */
+/*                                Exercitiul 3                                */
 /* -------------------------------------------------------------------------- */
+
+var LastToFirst = function(){
+    inputword = prompt("Type word");
+    var outpurword = "";
+    for ( var i = inputword.length - 1; i >= 0; i--){
+        outpurword= outpurword + inputword[i];
+    }
+   return outpurword;
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                Exercitiul 4                                */
+/* -------------------------------------------------------------------------- */
+
+const person = [
+    {name: "Catalin", age: 21, hobby:["cycling", "travel", "swim"]},
+    //{name: "Alexandru", age: 23, hobby:["books", "movies", "swim"]},
+    //{name: "Andrei", age: 24, hobby:["swin", "hinking", "workout"]},   
+];
+
+console.log("Numele: " + person[0].name);
+console.log("Varsta: " + person[0].age);
+console.log("Hobby: " + person[0].hobby.join(','));
+
+/* -------------------------------------------------------------------------- */
+/*                                Exercitiul 5                                */
+/* -------------------------------------------------------------------------- */
+
